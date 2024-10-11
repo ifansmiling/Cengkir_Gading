ffea8b19ff888137b7195684810a3360ae400b3d
ifan sultoni <ifansmiling66@gmail.com>
2024-10-12 00:42:05 +0700

    Update in layout

--- src/layouts/BerandaLayout.jsx
+++ src/layouts/BerandaLayout.jsx
@@ -4,21 +4,26 @@ import {
   FaHome,
   FaSignInAlt,
   FaUserPlus,
+  FaBars,
+  FaTimes,
+  FaUser,
+  FaPhone,
+  FaPaperPlane,
   FaFacebookF,
+  FaInstagram,
   FaWhatsapp,
   FaLinkedinIn,
-  FaInstagram,
-  FaPhone,
-  FaUser,
-  FaPaperPlane,
 } from "react-icons/fa";
 
 const BerandaLayout = ({ children }) => {
   const location = useLocation();
   const [activeLink, setActiveLink] = useState(location.pathname);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleClick = (path) => {
     setActiveLink(path);
+    setIsMenuOpen(false);
+    handleScrollToTop();
   };
 
   const handleScrollToTop = (event) => {
@@ -26,10 +31,14 @@ const BerandaLayout = ({ children }) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="bg-[#2e9e46] min-h-screen flex flex-col">
       {/* Navbar */}
-      <nav className="bg-white py-2 px-2 shadow-sm sticky top-0 z-50 flex items-center">
+      <nav className="bg-white py-2 px-2 shadow-sm sticky top-0 z-50 flex items-center justify-between">
         <div className="flex items-center">
           <a
             href="/"
@@ -37,26 +46,35 @@ const BerandaLayout = ({ children }) => {
               activeLink === "/"
                 ? "text-[#0B771D] text-xl"
                 : "text-gray-800 text-xl"
-            } hover:text-[#0B771D] font-dramatic-body`}
+            } hover:text-[#0B771D]`}
             onClick={() => handleClick("/")}
           >
-            <FaHome className="mr-1" />
-            Beranda
+            <FaHome className="mr-1 hidden md:block" />
+            <span className="hidden md:inline  font-dramatic-body">
+              Beranda
+            </span>
           </a>
         </div>
-        <div className="flex-1 flex justify-center ml-40">
+
+        <div className="flex justify-center items-center">
           <a href="/">
-            <img src="/logo1.png" alt="Company Logo" className="w-44 h-auto" />
+            <img
+              src="/logo1.png"
+              alt="Company Logo"
+              className="w-44 h-auto md:w-44 md:ml-32"
+            />
           </a>
         </div>
-        <div className="flex items-center">
+
+        {/* Links di layar besar */}
+        <div className="hidden md:flex items-center">
           <a
             href="/login"
-            className={`mx-4 font-bold transition-colors duration-300 flex items-center ${
+            className={`mx-4 font-bold transition-colors duration-300 flex items-center  font-dramatic-body ${
               activeLink === "/login"
                 ? "text-[#0B771D] text-xl"
                 : "text-gray-800 text-xl"
-            } hover:text-[#0B771D] font-dramatic-body`}
+            } hover:text-[#0B771D]`}
             onClick={() => handleClick("/login")}
           >
             <FaSignInAlt className="mr-1" />
@@ -64,17 +82,84 @@ const BerandaLayout = ({ children }) => {
           </a>
           <a
             href="/signup"
-            className={`mx-4 font-bold transition-colors duration-300 flex items-center ${
+            className={`mx-4 font-bold transition-colors duration-300 flex items-center font-dramatic-body ${
               activeLink === "/signup"
                 ? "text-[#0B771D] text-xl"
                 : "text-gray-800 text-xl"
-            } hover:text-[#0B771D] font-dramatic-body`}
+            } hover:text-[#0B771D]`}
             onClick={() => handleClick("/signup")}
           >
             <FaUserPlus className="mr-1" />
             Sign up
           </a>
         </div>
+
+        {/* Hamburger Menu Icon */}
+        <div className="md:hidden flex items-center">
+          <button
+            className={`text-gray-800 hover:text-[#0B771D] focus:outline-none transition-transform duration-300 ${
+              isMenuOpen ? "rotate-180" : "rotate-0"
+            }`}
+            onClick={toggleMenu}
+          >
+            {isMenuOpen ? <FaTimes size={28} /> : <FaBars size={28} />}
+          </button>
+        </div>
+
+        {/* Mobile Menu */}
+        {isMenuOpen && (
+          <div
+            className={`fixed top-0 right-0 h-full bg-white shadow-md py-4 z-50 transition-transform duration-500 ease-in-out transform ${
+              isMenuOpen ? "translate-x-0" : "translate-x-full"
+            } w-1/2 flex flex-col items-center space-y-4`}
+          >
+            <div className="w-full flex justify-end px-4">
+              <button
+                className="text-gray-800 hover:text-[#0B771D] focus:outline-none"
+                onClick={toggleMenu}
+              >
+                <FaTimes size={28} />
+              </button>
+            </div>
+
+            <a
+              href="/"
+              className={`font-bold transition-colors duration-300 flex items-center font-dramatic-body ${
+                activeLink === "/"
+                  ? "text-[#0B771D] text-lg"
+                  : "text-gray-800 text-lg"
+              } hover:text-[#0B771D]`}
+              onClick={() => handleClick("/")}
+            >
+              <FaHome className="mr-1" />
+              Beranda
+            </a>
+            <a
+              href="/login"
+              className={`font-bold transition-colors duration-300 flex items-center font-dramatic-body ${
+                activeLink === "/login"
+                  ? "text-[#0B771D] text-lg"
+                  : "text-gray-800 text-lg"
+              } hover:text-[#0B771D]`}
+              onClick={() => handleClick("/login")}
+            >
+              <FaSignInAlt className="mr-1" />
+              Login
+            </a>
+            <a
+              href="/signup"
+              className={`font-bold transition-colors duration-300 flex items-center font-dramatic-body ${
+                activeLink === "/signup"
+                  ? "text-[#0B771D] text-lg"
+                  : "text-gray-800 text-lg"
+              } hover:text-[#0B771D]`}
+              onClick={() => handleClick("/signup")}
+            >
+              <FaUserPlus className="mr-1" />
+              Sign up
+            </a>
+          </div>
+        )}
       </nav>
 
       {/* Main Content */}
@@ -83,9 +168,9 @@ const BerandaLayout = ({ children }) => {
       {/* Footer */}
       <footer>
         <div className="bg-custom-gray py-4 px-4 text-white mt-auto shadow-md">
-          <div className="container mx-auto flex justify-between items-start space-x-6">
+          <div className="container mx-auto flex flex-col md:flex-row justify-between items-start md:space-x-6 space-y-6 md:space-y-0">
             {/* Section Ajakan Bergabung */}
-            <div className="w-1/3">
+            <div className="w-full md:w-1/3">
               <h2 className="text-xl font-footer-heading font-bold mb-4">
                 Silahkan Daftar dan Bergabung dengan Kami!
               </h2>
@@ -119,7 +204,7 @@ const BerandaLayout = ({ children }) => {
             </div>
 
             {/* Section Info Kontak */}
-            <div className="w-1/3 flex justify-center items-center mt-4 mr-3">
+            <div className="w-full md:w-1/3 flex justify-center items-center mt-4 md:mt-0">
               <img src="/logo.png" alt="Footer Logo" className="w-24 h-auto" />
               <div className="ml-4 text-left">
                 <p className="font-footer-body text-sm">
@@ -147,7 +232,7 @@ const BerandaLayout = ({ children }) => {
             </div>
 
             {/* Section Media Sosial */}
-            <div className="w-1/4 text-center mr-4">
+            <div className="w-full md:w-1/4 text-center">
               <h2 className="text-xl font-footer-heading mb-4">Follow Us</h2>
               <div className="flex justify-center space-x-3">
                 <a
